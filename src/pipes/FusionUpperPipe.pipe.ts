7e6cffaa918e25e0943186d012a676d182dccb6d
TaherJerbi <jerbi.taherr@gmail.com>
2022-03-01 23:20:55 +0100

    fix: bad request in FusionUpperPipe

--- src/pipes/FusionUpperPipe.pipe.ts
+++ src/pipes/FusionUpperPipe.pipe.ts
@@ -1,10 +1,19 @@
-import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
 export class FusionUpperPipe implements PipeTransform {
-  transform(value: { skills }, metadata: ArgumentMetadata): string {
+  transform(
+    value: { skills: Array<string> },
+    metadata: ArgumentMetadata,
+  ): string {
     console.log(
       '🚀 ~ file: FusionUpperPipe.pipe.ts ~ line 4 ~ FusionUpperPipe ~ transform ~ metadata',
       metadata,
     );
+    if (!value?.skills)
+      throw new BadRequestException('expected an array of skills');
     return value.skills.map((skill) => skill.toUpperCase()).join('-');
   }
 }
